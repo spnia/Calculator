642b432dfe5d2e13abcc07b7aa69e513f8f47335
spnia <chco41432@gmail.com>
2024-01-03 18:34:51 +0900

    Commit

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,4 +1,10 @@
 module.exports = {
   root: true,
   extends: '@react-native',
+  rules: {
+    'prettier/prettier': 0,
+    '@typescript-eslint/no-unused-vars': 0,
+    'comma-dangle': 0,
+    'quotes': 0
+  }
 };
